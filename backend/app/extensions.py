8979b788253561aa12b643d4be64afd33eeadfb0
lesuuda <lesuudaljamani@gmail.com>
2024-07-13 22:44:55 +0300

    implemented most functionalities with a few bugs to address

--- backend/app/extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager
-
-db = SQLAlchemy()
-migrate = Migrate()
-bcrypt = Bcrypt()
-jwt = JWTManager()
