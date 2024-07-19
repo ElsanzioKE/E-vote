0beac622834dbdf3328efc67f55bb7c68cc79411
lesuuda <lesuudaljamani@gmail.com>
2024-07-19 23:41:04 +0300

    reconfigured to use mysqlite3

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 DB_PATH = os.path.join(os.path.dirname(__file__), 'votr.db')
 SECRET_KEY = 'development key'  # keep this key secret during production
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://evote_dev:evote_pwd@localhost/evote'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(DB_PATH)
 CELERY_BROKER = 'amqp://guest@localhost//'
 CELERY_RESULT_BACKEND = 'rpc://'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
